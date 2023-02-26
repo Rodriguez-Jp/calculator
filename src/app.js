d103abc4bc72147fc27c0714a3440a9bb6f26c77
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-02-26 13:19:20 -0500

    Added Del, %, sign functionality, and correct bug that doesnt let add 2 decimal numbers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".op");
 const screen = document.querySelector("#screen");
-let cont = 0;
+let dotValidator = true;
 
 //EventListeners
 
@@ -27,17 +27,43 @@ function loadEventListeners() {
 
 //Get the reference and display his value (Numbers)
 function displayNumbers(ref) {
-  const input = ref.getAttribute("value");
-  const conditions = ["+", "-", "/", "x", "%", "del"];
+  const input = validateNumbers(ref);
+  screen.innerHTML += input;
+}
 
+//Validates the number input
+function validateNumbers(ref) {
+  const input = ref.getAttribute("value");
+
+  //Validation for dot (avoid multiple dots in the same number)
+  if (!dotValidator && input === ".") {
+    dotValidator = true;
+    return input;
+  }
   if (
     (input === "." && screen.textContent.includes(".")) ||
     (input === "." && screen.textContent === "")
   ) {
-    return;
+    return "";
   }
 
-  screen.innerHTML += input;
+  //Validation for zero (Avoid left zeros)
+  if (
+    input === "0" &&
+    screen.textContent.length === 1 &&
+    screen.textContent[0] === "0"
+  ) {
+    return "";
+  }
+  if (
+    screen.textContent[0] === "0" &&
+    input !== "." &&
+    screen.textContent.length < 2
+  ) {
+    screen.textContent = input;
+    return "";
+  }
+  return input;
 }
 
 //Displays the operator on the screen
@@ -53,6 +79,26 @@ function displayOperator(ref) {
     clearScreen();
   }
 
+  //If users press the +/- button (**Need some work**)
+  if (ref.getAttribute("value") === "sign") {
+    let newNum = changeSign(screen.textContent);
+    screen.textContent = newNum;
+    return;
+  }
+
+  //If users press the del button
+  if (ref.getAttribute("value") === "del") {
+    deleteNum();
+    return;
+  }
+
+  //If user press the percetange button (**Need some work**)
+  if (ref.getAttribute("value") === "%") {
+    let percentageNum = percentage(screen.textContent);
+    screen.textContent = percentageNum;
+    return;
+  }
+
   const input = validateOperator(ref);
   screen.innerHTML += input;
 }
@@ -61,7 +107,7 @@ function displayOperator(ref) {
 function validateOperator(ref) {
   const input = ref.getAttribute("value");
   const last_input = screen.textContent.slice(-1);
-  const conditions = ["+", "-", "/", "x", "%", "del"];
+  const conditions = ["+", "-", "/", "*", "%", "del"];
 
   if (screen.textContent.trim() === "") {
     return "";
@@ -71,6 +117,7 @@ function validateOperator(ref) {
     return "";
   }
 
+  dotValidator = false;
   return input;
 }
 
@@ -83,4 +130,25 @@ function calculate() {
 //Clear the screen
 function clearScreen() {
   screen.textContent = "";
+}
+
+//Change the sign of the number
+function changeSign(num) {
+  num = parseFloat(num) * -1;
+  const numString = String(num);
+  return numString;
+}
+
+//Delete numbers
+function deleteNum() {
+  screen.textContent = screen.textContent.slice(
+    0,
+    screen.textContent.length - 1
+  );
+}
+
+//Converts the number in percentage
+function percentage(num) {
+  let newNum = parseFloat(num) / 100;
+  return String(newNum);
 }
